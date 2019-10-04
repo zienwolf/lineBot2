c9b8def48a02b17ffc88a4f035113f7c13ed6d18
zienwolf <54126732+zienwolf@users.noreply.github.com>
2019-10-04 15:46:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken'] 
+    userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'ดี') :
         sendText(user,'จ่ะ')
